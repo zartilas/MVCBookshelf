36d112a3ff13b95455c610e94d7264951d7ba512
Νικόλας Πατέρας <nikolas.98@live.com>
2021-01-15 17:25:04 +0200

    error 404 page

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using MVCBookshelf.Controllers;
+using System;
+using System.Web;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -13,5 +16,47 @@ namespace MVCBookshelf
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception lastErrorInfo = Server.GetLastError();
+            Exception errorInfo = null;
+
+            bool isNotFound = false;
+            if (lastErrorInfo != null)
+            {
+                errorInfo = lastErrorInfo.GetBaseException();
+                var error = errorInfo as HttpException;
+                if (error != null)
+                    isNotFound = error.GetHttpCode() == (int)System.Net.HttpStatusCode.NotFound;
+            }
+            if (isNotFound)
+            {
+                Server.ClearError();
+                Response.Redirect("~/Error/NotFound");// Do what you need to render in view
+            }
+        }
+
+/*        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            Response.Clear();
+            HttpException httpException = exception as HttpException;
+            RouteData routeData = new RouteData();
+            routeData.Values.Add("controller", "Error");
+            if (httpException != null)
+            {
+                switch(httpException.GetHttpCode())
+                {
+                    case 404:
+                        routeData.Values.Add("action", "NotFound");
+                        break;
+                }
+                Server.ClearError();
+                Response.TrySkipIisCustomErrors = true;
+            }
+            IController errorController = new ErrorController();
+            errorController.Execute(new RequestContext(
+                new HttpContextWrapper(Context), routeData));
+        }*/
     }
 }
